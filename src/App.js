08d75170b0b2b3a1e6846506d79caf100f258e9f
yyyayee <tanida.yt@gmail.com>
2024-08-05 16:31:35 +0700

    func component
    func component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Header, Content, Footer } from './func-components'
 import Calendar from './class-components'
 
 export default function App() {
   return (
     <>
+    <Header/>
     <p><center><Calendar/></center></p>
+    <Content/>
+    <Footer/>
     </>
   )
 }
