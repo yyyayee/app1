748d982867ed4cee62c002f551a3e15ce635a898
yyyayee <tanida.yt@gmail.com>
2024-08-12 23:48:44 +0700

    this - Arrow Function
    this - Arrow Function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react'
-import { Header, Content, Footer } from './func-components'
-import Calendar from './class-components'
+import Button from './class-components'
 
-export default function App() {
-  return (
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
-  )
+function App() {
+  return <Button/>
 }
+
+export default App
--- src/class-components.js
+++ src/class-components.js
@@ -1,18 +1,15 @@
-import React from "react";
+import React, {Component} from "react";
 
-export default class Calendar extends React.Component {
-    getDate() {
-        const dayNames = ['อาทิตย์', 'จัทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์']
-        const monthNames = ['มกราคม', 'กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม']
-        const date = new Date()
-        const weekDay = dayNames[date.getDay()]
-        const day = date.getDate()
-        const month = monthNames[date.getMonth()]
-        const year = date.getFullYear() + 543
-        return `วัน${weekDay} ที่ ${day} ${month} ${year}` //กด window + ปุ่มเปลี่ยนภาษา
+export default class Button extends Component {
+    showAlert(msg) {
+        alert(msg)
+    }
+
+    onClickButtonOK = () => {       //Arrow Function
+        this.showAlert('Hello')
     }
 
     render() {
-        return <div>{this.getDate()}</div>
+        return <button onClick={this.onClickButtonOK}>OK</button>
     }
 }
