56c144bc1d0c02fd522e847e98e570702ef52c1c
yyyayee <tanida.yt@gmail.com>
2024-08-16 17:55:57 +0700

    event data
    event data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Calculator2 } from './calculator'
+import React from "react";
+import { EventData2 } from "./event-data";
 
 export default function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
