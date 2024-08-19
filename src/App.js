ea29a3418bc5a4baaf32d7d1181c99150a5b84dc
yyyayee <tanida.yt@gmail.com>
2024-08-19 10:01:00 +0700

    refs-func
    refs-func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { EventData2 } from "./event-data";
+import RefsFunc from "./refs-func";
 
 export default function App() {
-  return <EventData2/>
+  return <RefsFunc/>
 }
