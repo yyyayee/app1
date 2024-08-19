a399578bf6887cec7c3ce102d166db8403c7066a
yyyayee <tanida.yt@gmail.com>
2024-08-19 11:10:05 +0700

    state-func
    state-func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import RefsFunc from "./refs-func";
+import MessageBox from "./state-func";
 
 export default function App() {
-  return <RefsFunc/>
+  return <MessageBox/>
 }
