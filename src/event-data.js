cde9bfca3a031883ab5e1737525e4d89c64c42a4
yyyayee <tanida.yt@gmail.com>
2024-08-25 23:50:44 +0700

    Edit event-data
    Edit event-data

--- src/event-data.js
+++ src/event-data.js
@@ -17,7 +17,7 @@ export function EventData2() {
         <div style={{textAlign:'center', marginTop:20}}>
             <button onClick={(ev) => onClickStartStop(ev)}>Start</button><br/><br/>
             <button onClick={(ev) => onClickAdd(ev)}>10 + 20</button><br/><br/>
-            <button onClick={onClickAdd.bind(this)}>30 * 40</button><br/><br/>
+            <button onClick={(ev) => onClickAdd(ev)}>30 * 40</button><br/><br/>
         </div>
     )
 }
